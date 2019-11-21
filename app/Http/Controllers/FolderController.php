95badc4b83264046a56cf332b76999056bad4c0c
tenra1115 <katsuten1115@icloud.com>
2019-11-21 11:28:14 +0900

    folder-edit

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -7,6 +7,7 @@ use App\Folder;
 // フォームリクエストを読み込み
 use App\Http\Requests\CreateFolder;
 use Illuminate\Http\Request;
+use App\Http\Requests\EditFolder;
 use Illuminate\Support\Facades\Auth;
 
 class FolderController extends Controller
@@ -27,6 +28,23 @@ class FolderController extends Controller
         Auth::user()->folders()->save($folder);
 
         // iはどこで必要とされているのか？tasks.indexの何で必要なのか？
+        return redirect()->route('tasks.index', ['i' => $folder->id,]);
+    }
+
+    public function showEditForm(Folder $folder)
+    {
+        // $this->check($folder);
+
+        // , ['folder' => $folder,]が無いとfolderがundefindってなる
+        return view('folders/edit', ['folder' => $folder,]);
+
+    }
+
+    public function edit(Folder $folder, CreateFolder $request)
+    {
+        $folder->title = $request->title;
+        $folder->save();
+        // iはtasks.indexに帰るときに/folders/{folder}/tasksの{folder}に入る値を決めている
         return redirect()->route('tasks.index', ['i' => $folder->id,]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,18 @@ Route::group(['middleware' => 'auth'], function() {
   // nameでコントローラーアクションに対しての名前をつけることができるので便利
   Route::get('/', 'HomeController@index')->name('home');
   
-  Route::get('/folders/{i}/tasks', 'TaskController@index')->name('tasks.index');
+  Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
   
   Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
   Route::post('/folders/create', 'FolderController@create');
 
-  Route::get('/folders/{i}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
-  Route::post('/folders/{i}/tasks/create', 'TaskController@create');
+  Route::get('/folders/{folder}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+  Route::post('/folders/{folder}/tasks/create', 'TaskController@create');
   
   // {}の中身はなんでも良いが、コントローラーでアクションを動かす際に呼び出す必要がある(ここの値を使う)
-  Route::get('/folders/{i}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
-  Route::post('/folders/{i}/tasks/{task_id}/edit', 'TaskController@edit');
+  Route::get('/folders/{folder}/tasks/{task}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+  Route::post('/folders/{folder}/tasks/{task}/edit', 'TaskController@edit');
+
+  Route::get('/folders/{folder}/edit', 'FolderController@showEditForm')->name('folders.edit');
+  Route::post('/folders/{folder}/edit', 'FolderController@edit');
 });
